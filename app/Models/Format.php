c0e28a66c934d019ef7f55ddd7c7b214e1edcce7
Cade Wilson <cade25wilson@gmail.com>
2024-04-20 04:57:52 +0000

    database schema

--- app/Models/Format.php
+++ app/Models/Format.php
@@ -9,7 +9,7 @@ use Illuminate\Database\Eloquent\Model;
 class Format extends Model
 {
     use HasFactory;
-
+    protected $table = 'image_formats';
     protected $fillable = [
         'id',
         'name',
