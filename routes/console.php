f3b5509524fef9d536d15eedcdb86f9fe2c67cc9
Cade Wilson <cade25wilson@gmail.com>
2024-04-21 05:37:42 +0000

    added task to clear files older than 24 hours

--- routes/console.php
+++ routes/console.php
@@ -6,3 +6,24 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
+
+Artisan::command('clearoldfiles', function() {
+    $files = glob(storage_path('app/audio/*'));
+    $now = time();
+    foreach ($files as $file) {
+        if (is_file($file)) {
+            if ($now - filemtime($file) >= 60 * 60 * 24) {
+                unlink($file);
+            }
+        }
+    }
+
+    $files = glob(storage_path('app/images/*'));
+    foreach ($files as $file) {
+        if (is_file($file)) {
+            if ($now - filemtime($file) >= 60 * 60 * 24) {
+                unlink($file);
+            }
+        }
+    }
+})->purpose('Clear files older than 24 hours')->everyThirtyMinutes();
