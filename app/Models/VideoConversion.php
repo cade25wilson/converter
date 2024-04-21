994a1ca636b35e9e9b8ae5e2e09e5a14b2c35600
Cade Wilson <cade25wilson@gmail.com>
2024-04-21 21:15:13 +0000

    video conversion

---
+++ app/Models/VideoConversion.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class VideoConversion extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['original_name', 'original_format', 'converted_format', 'status', 'guid', 'converted_name'];
+
+    public function originalFormat()
+    {
+        return $this->belongsTo(VideoFormat::class, 'original_format');
+    }
+
+    public function convertedFormat()
+    {
+        return $this->belongsTo(VideoFormat::class, 'converted_format');
+    }
+}
