779c24440d46734b5934886c1bec095c9f65643c
Cade Wilson <cade25wilson@gmail.com>
2024-05-04 05:50:49 +0000

    footer

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -29,12 +29,45 @@
     <div class="container">
         <router-view></router-view>
     </div>
-    <footer class="footer">
-        <div class="container">
-            <router-link to="/privacy" class="text-default">Privacy</router-link>
-            <router-link to="/faq" class="text-default">FAQ</router-link>
-            <router-link to="/contact" class="text-default">Contact</router-link>
-            
+      <!-- Site footer -->
+      <footer class="site-footer">
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-12 col-md-6">
+            <h6>About</h6>
+            <p class="text-justify">Transforming multimedia and data effortlessly, our website serves as a versatile hub for converting audio, video, image, and spreadsheet file formats with seamless precision. Whether it's refining audio clips, enhancing video quality, optimizing images, or streamlining data in spreadsheets, our platform offers a user-friendly experience, ensuring efficiency and accuracy in every conversion. Embracing cutting-edge technology, we empower users to effortlessly adapt their digital content to meet diverse needs, facilitating smooth transitions across formats with just a few clicks.</p>
+          </div>
+
+          <div class="col-xs-6 col-md-3">
+            <h6>Converters</h6>
+            <ul class="footer-links">
+              <li><router-link to="/images">Images</router-link></li>
+              <li><router-link to="/audios">Audio</router-link></li>
+              <li><router-link to="/videos">Video</router-link></li>
+              <li><router-link to="/spreadsheets">Spreadsheets</router-link></li>
+            </ul>
+          </div>
+          <div class="col-xs-6 col-md-3">
+            <h6>Quick Links</h6>
+            <ul class="footer-links">
+              <li><router-link to="/about">About</router-link></li>
+              <li><router-link to="/contact">Contact Us</router-link></li>
+              <li><router-link to="/donate">Contribute</router-link></li>
+              <li><router-link to="/privacy">Privacy Policy</router-link></li>
+              <li><router-link to="/sitemap.xml">Sitemap</router-link></li>
+            </ul>
+          </div>
         </div>
-    </footer>
+        <hr>
+      </div>
+      <div class="container">
+        <div class="row">
+          <div class="col-md-8 col-sm-6 col-xs-12">
+            <p class="copyright-text">Copyright &copy; 2024 All Rights Reserved by 
+            <a href="#">File Converter Services</a>.
+            </p>
+          </div>
+        </div>
+      </div>
+</footer>
 </template>
