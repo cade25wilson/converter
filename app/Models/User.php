f76ec0f37c4eb538629855ca1ff86d31ae87df3d
Cade Wilson <cade25wilson@gmail.com>
2024-05-05 23:07:28 +0000

    mailinglist created

--- app/Models/User.php
+++ app/Models/User.php
@@ -46,4 +46,9 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function mailingList()
+    {
+        return $this->hasOne(MailingList::class);
+    }
 }
