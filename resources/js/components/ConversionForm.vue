becb037b0611f0427ef2eb24622b750309094626
Cade Wilson <cade25wilson@gmail.com>
2024-05-20 01:52:13 +0000

    multiple selects

--- resources/js/components/ConversionForm.vue
+++ resources/js/components/ConversionForm.vue
@@ -8,22 +8,29 @@
                     @drop="dropFiles"
                     v-if="files.length === 0"
                 >
-                <label for="file" class="custum-file-upload mr-3" >
-                    <div class="icon">
+                <label class="custum-file-upload mr-3">
+                    <div class="icon" @click="triggerFileSelection">
                         <svg viewBox="0 0 24 24" fill="" xmlns="http://www.w3.org/2000/svg">
                             <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
                             <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
                             <g id="SVGRepo_iconCarrier">
                                 <path fill-rule="evenodd" clip-rule="evenodd"
                                     d="M10 1C9.73478 1 9.48043 1.10536 9.29289 1.29289L3.29289 7.29289C3.10536 7.48043 3 7.73478 3 8V20C3 21.6569 4.34315 23 6 23H7C7.55228 23 8 22.5523 8 22C8 21.4477 7.55228 21 7 21H6C5.44772 21 5 20.5523 5 20V9H10C10.5523 9 11 8.55228 11 8V3H18C18.5523 3 19 3.44772 19 4V9C19 9.55228 19.4477 10 20 10C20.5523 10 21 9.55228 21 9V4C21 2.34315 19.6569 1 18 1H10ZM9 7H6.41421L9 4.41421V7ZM14 15.5C14 14.1193 15.1193 13 16.5 13C17.8807 13 19 14.1193 19 15.5V16V17H20C21.1046 17 22 17.8954 22 19C22 20.1046 21.1046 21 20 21H13C11.8954 21 11 20.1046 11 19C11 17.8954 11.8954 17 13 17H14V16V15.5ZM16.5 11C14.142 11 12.2076 12.8136 12.0156 15.122C10.2825 15.5606 9 17.1305 9 19C9 21.2091 10.7909 23 13 23H20C22.2091 23 24 21.2091 24 19C24 17.1305 22.7175 15.5606 20.9844 15.122C20.7924 12.8136 18.858 11 16.5 11Z"
-                                    fill=""></path>
+                                    fill="">
+                                </path>
                             </g>
                         </svg>
                     </div>
-                    <div class="text">
-                        <span class="text-default">Click to upload image</span>
+                    <div class="text" @mouseleave="showList = false">
+                        <span class="text-default" @click="triggerFileSelection">Click to upload image</span>
+                        <span class="arrow-down" @mouseover="showList = true">&#x25BC;</span>
+                        <ul class="dropdown-list" v-show="showList">
+                            <li @click.prevent="console.log('test');">Dropbox</li>                            
+                            <li @click.prevent="console.log('test');">Google Drive</li>
+                            <li @click.prevent="console.log('test');">Onedrive</li>
+                        </ul>
                     </div>
-                    <input type="file" id="file" v-on:change="files = Array.from($event.target.files)" multiple>
+                    <input type="file" ref="fileInput" v-on:change="files = Array.from($event.target.files)" multiple>
                 </label>
                 </div>
                 <div style="height: 300px; overflow-y: auto;" v-else>
@@ -149,6 +156,7 @@ export default {
             conversionStatus: '',
             resize: false,
             showFormats: false,
+            showList: false,
         }
     },
     created() {
@@ -175,6 +183,10 @@ export default {
                     'video': [],
                 }));
             }
+        },
+        triggerFileSelection() {
+            console.log('triggerfileselection called');
+            this.$refs.fileInput.click();
         },
         removeFile(index){
             this.files.splice(index, 1);
@@ -370,7 +382,31 @@ export default {
   transition: 300ms ease;
 }
 
-/* Inspired by twitter.com/marina_uiux */
+.dropdown {
+  position: relative;
+  display: inline-block;
+}
+
+.arrow-down {
+  margin-left: 5px;
+}
+
+.dropdown-list {
+  /* display: none; */
+  position: absolute;
+  background-color: #f9f9f9;
+  min-width: 160px;
+  box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+  padding: 12px 16px;
+  z-index: 1;
+}
+
+.dropdown-list li {
+  color: black;
+  padding: 12px 16px;
+  text-decoration: none;
+  display: block;
+}
 
 .button {
   font-size: 17px;
