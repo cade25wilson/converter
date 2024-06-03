46bc3b62b50a27e678b903335082dc87bc3ab136
Cade Wilson <cade25wilson@gmail.com>
2024-06-03 03:55:30 +0000

    multiple ebook conversions

--- app/Services/EbookConverterService.php
+++ app/Services/EbookConverterService.php
@@ -3,9 +3,11 @@
 namespace App\Services;
 
 use App\Jobs\ConvertSingleEbook;
+use App\Jobs\ConvertMultipleEbook;
 use App\Models\EbookConversion;
 use App\Models\EbookFormat;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 
 class EbookConverterService
 {
@@ -37,8 +39,32 @@ class EbookConverterService
         return $data['guid'];
     }
 
-    public function MultipleConvert()
+    public function MultipleConvert(): string
     {
+        $guid = str::uuid();
+        $ebookFiles = $this->request->file('ebook');
+        $ebookConversion = [];
 
+        foreach ($ebookFiles as $ebook) {
+            $originalName = $ebook->getClientOriginalName();
+            $ebook->storeAs('ebook/' . $guid . '/', $originalName);
+            $fileSize = $ebook->getSize();
+
+            $format = EbookFormat::where('id', $this->request->input('format'))->first();
+            $convertedFormat = $format->extension;
+
+            $ebookConversion[] = EbookConversion::create([
+                'original_name' => $originalName,
+                'converted_format' => $format->id,
+                'converted_name' => pathinfo($originalName, PATHINFO_FILENAME) . '.' . $convertedFormat,
+                'status' => 'pending',
+                'guid' => $guid,
+                'file_size' => $fileSize,
+            ]);
+        }
+
+        ConvertMultipleEbook::dispatch($guid, $convertedFormat);
+
+        return $guid;
     }   
 }
